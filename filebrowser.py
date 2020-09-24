f2a13c5a27c05e8d7cc23f625cd296e054d5edcf
michaelPotter <mpotter.dev@gmail.com>
2020-09-23 19:24:39 -0700

    add filebrowser stub

---
+++ filebrowser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# filebrowser.py
+#
+# Michael Potter
+# 2020-07-24
+
+from pager import Pager
+import linepager
+from models import *
+from util import *
+import shapes
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
